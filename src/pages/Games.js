5838656876cc67533ce2d049cecac492062583c5
Kimberly Modeste <KimberlyJModeste@gmail.com>
2022-03-21 17:01:14 -0400

    Edits to dev

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -1,9 +1,11 @@
 import React from "react";
+import MenuBar from "../components/MenuBar";
 
 function Games(){
 
     return(
         <div >
+            <MenuBar color="black" inverted={false}/>
             <h1>Work In Progress</h1>
         </div>
     )
