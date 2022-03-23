7e75b7be8fa8b8f3152233991abae43a6c4da5b7
Kimberly Modeste <KimberlyJModeste@gmail.com>
2022-03-23 15:09:38 -0400

    Revert "Edited hoe page"
    This reverts commit 8a315c4d81e7d24142c5e64f227bd0612949a894.

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -8,7 +8,7 @@ function Resume(){
 return( 
     <div > 
         <MenuBar inverted={true} />
-        <iframe title='MasterResume' src={MasRes} width="100%" height="1050px"/> 
+        <iframe title='MasterResume' src={MasRes} width="100%" height="500px"/> 
     </div>
 )
 }
