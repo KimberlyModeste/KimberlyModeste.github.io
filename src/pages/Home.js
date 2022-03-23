8a315c4d81e7d24142c5e64f227bd0612949a894
Kimberly Modeste <KimberlyJModeste@gmail.com>
2022-03-23 14:43:15 -0400

    Edited hoe page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,21 +11,15 @@ return(
     <Link />
     <div className = "homediv" >
     
-    <img src={headshot} alt='My face but in home' style={{padding: '10px', width: "150px", height: "150px", borderRadius: 400/ 2}} />
+    <img src={headshot} alt='My face but in home' style={{padding: '10px', width: "250px", height: "250px", borderRadius: 400/ 2}} />
     <p>
-        <div style={{width: "300px", overflow: "visible"}}>
+        <div className='hometext'>
             <strong>Hi! I'm Kimberly!</strong> 
             <br/><br/>
                 I'm a computer scientist that likes making websites, games, drawing and learning languages! 
                 I also like cryptography and solving puzzles! I have some of the games I worked on the games
                 tab, so feel free to play them. 
         </div>
-        {/* <h1 style={{color:'black'}}>Hi! I'm Kimberly!</h1> */}
-        {/* <h4>
-            I'm a computer scientist that likes making websites, games, drawing and learning languages! <br/>
-            I also like cryptography and solving puzzles! I have some of the games I worked on the games <br/>
-            tab, so feel free to play them. 
-        </h4> */}
     </p>
     </div>
     </div>
