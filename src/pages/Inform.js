a314c727a8af1fea6c8c4daf57470c5bbd7d8616
KimberlyModeste <54868542+KimberlyModeste@users.noreply.github.com>
2024-04-26 18:34:46 -0400

    Updated Master Resume

---
+++ src/pages/Inform.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Links from '../components/Links';
+import MenuBar from '../components/MenuBar';
+
+function Inform(){
+
+return(
+  <div> 
+    <MenuBar color="black" inverted={false}/>
+    <Links />
+    <div className = "informdiv" >
+      
+    </div>
+  </div>
+    )
+}
+export default Inform
