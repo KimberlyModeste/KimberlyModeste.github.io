f24300df04670ec4e855bcb57d37a9f08ec7cd89
KimberlyModeste <54868542+KimberlyModeste@users.noreply.github.com>
2024-05-15 17:31:21 -0400

    Changed the size o

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -24,7 +24,7 @@ function Play(){
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 3,
-      slidesToSlide: 2 // optional, default to 1.
+      slidesToSlide: 1 // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
